922036b6003c9c5b63ee5e6f560a4798c6a50a6b
Tim Burgess <timburgess@mac.com>
2015-11-23 09:15:18 +1000

    Add in default Framework project

---
+++ Source/Swix.h
@@ -0,0 +1,19 @@
+//
+//  Swix.h
+//  Swix
+//
+//  Created by Tim Burgess on 23/11/2015.
+//  Copyright © 2015 Tim Burgess. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Swix.
+FOUNDATION_EXPORT double SwixVersionNumber;
+
+//! Project version string for Swix.
+FOUNDATION_EXPORT const unsigned char SwixVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swix/PublicHeader.h>
+
+
